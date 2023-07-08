847346d5571152f51ebfb7b2da5cf1d25f94df02
Nathan <side_nathan@hotmail.com>
2023-07-08 15:03:18 -0400

    Add basic hover functionality to grid

--- javascript.js
+++ javascript.js
@@ -5,5 +5,12 @@ for (let i = 0; i < 16; i++) {
     boxes[i] = document.createElement('div');
     boxes[i].className = 'flexSquare';
     boxes[i].id = 'flexSquare' + (i + 1);
+    boxes[i].addEventListener("mouseover", () => {
+        boxes[i].style.backgroundColor = "blue";
+
+        setTimeout( () => {
+            boxes[i].style.backgroundColor = "white";
+        }, 500);
+    });
     grid.appendChild(boxes[i]);
 }
