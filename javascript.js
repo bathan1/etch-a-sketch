a2eadd95c5f3a0c90a84f0279bf9924146f41c37
Nathan <side_nathan@hotmail.com>
2023-07-08 15:28:25 -0400

    Add random color generator

--- javascript.js
+++ javascript.js
@@ -1,12 +1,16 @@
 const grid = document.getElementById('grid');
 let boxes = [];
 
+function randomColorGenerator() {
+    return Math.floor(Math.random()*16777215).toString(16);
+}
+
 for (let i = 0; i < 16; i++) {
     boxes[i] = document.createElement('div');
     boxes[i].className = 'flexSquare';
     boxes[i].id = 'flexSquare' + (i + 1);
     boxes[i].addEventListener("mouseover", () => {
-        boxes[i].style.backgroundColor = "blue";
+        boxes[i].style.backgroundColor = "#" + randomColorGenerator();
 
         setTimeout( () => {
             boxes[i].style.backgroundColor = "white";
